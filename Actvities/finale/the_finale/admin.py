89a8c18ce8fef554fe205b099693375d15c61b23
IyanuKwent <olandria.iankent@gmail.com>
2025-03-03 19:27:06 +0800

    removed commend

--- Actvities/finale/the_finale/admin.py
+++ Actvities/finale/the_finale/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Vehicle, Booking, Review, LoyaltyProgram, Location
 
-# Register your models here.
+
 admin.site.register(Vehicle)
 admin.site.register(Booking)
 admin.site.register(Review)
