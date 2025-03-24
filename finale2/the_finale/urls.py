8276835de912e9224eaf4b215bc22abb0463ca7f
Ian Kent <olandria.iankent@gmail.com>
2025-03-24 09:28:34 +0800

    url name change

--- finale2/the_finale/urls.py
+++ finale2/the_finale/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostListCreateAPIView, PostRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
-    path('the_finale/', PostListCreateAPIView.as_view(), name='list-create-post'),
-    path('the-finale/<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view(), name='retrieve-update-destroy-post'),
+    path('admin/', PostListCreateAPIView.as_view(), name='list-create-post'),
+    path('the_finale/<int:pk>/', PostRetrieveUpdateDestroyAPIView.as_view(), name='retrieve-update-destroy-post'),
 ]
